0c2be1b78dfccc345893a325a3abb1fafdf3f0d4
julianlen <julianlen@gmail.com>
2019-05-07 10:30:58 -0300

    delete unused code

--- rskj-core/src/main/java/co/rsk/SnappyMetrics.java
+++ rskj-core/src/main/java/co/rsk/SnappyMetrics.java
@@ -5,7 +5,6 @@ import org.ethereum.core.Block;
 import org.ethereum.core.Blockchain;
 import org.ethereum.db.BlockInformation;
 import org.ethereum.db.BlockStore;
-import org.ethereum.util.ByteUtil;
 
 
 import java.util.List;
@@ -91,32 +90,7 @@ public class SnappyMetrics {
         System.out.println("Total time: " + totalTime);
         System.out.println("Average time: " + totalTime/times);
     }
-/*
-    public void compareWrites() {
 
-        long snappyBlockNumber = snappyDummyBlockchain.getBestBlock().getNumber() + 1;
-        long normalBlockNumber = normalDummyBlockchain.getBestBlock().getNumber() + 1;
-        long snappyTime = 0;
-        long normalTime = 0;
-
-        for (int i = 0; i < VALUES_TO_GENERATE; i++) {
-            Block snappyBlock = blockchainWithSnappy.getChainBlockByNumber(snappyBlockNumber++);
-            Block normalBlock = blockchain.getChainBlockByNumber(normalBlockNumber++);
-
-            long saveTimeSnappy = System.nanoTime();
-            snappyDummyBlockchain.tryToConnect(snappyBlock);
-            snappyTime += System.nanoTime() - saveTimeSnappy;
-
-            long saveTime = System.nanoTime();
-            normalDummyBlockchain.tryToConnect(normalBlock);
-            normalTime += System.nanoTime() - saveTime;
-        }
-
-        System.out.println("Writing Snappy time: " + snappyTime);
-        System.out.println("Writing Normal time: " + normalTime);
-
-    }
-   */
 
     private long measureNormalWrites() {
         long normalBlockNumber = normalDummyBlockchain.getBestBlock().getNumber() + 1;
@@ -186,103 +160,6 @@ public class SnappyMetrics {
             }
         }
         return snappyTime;
-    }
-
-
-    public long[] compareReads () {
-        Random valueSource = new Random();
-        IntStream blockNumbers = valueSource.ints(VALUES_TO_GENERATE, MIN, MAX);
-
-        long snappyTime = 0;
-        long normalTime = 0;
-        int quantity = 1;
-
-
-        for (int anInt : blockNumbers.toArray()) {
-
-            if (quantity % 100 == 0) {
-                System.out.println("Read block number " + quantity);
-            }
-
-            List<BlockInformation> bInfo = blockchain.getBlocksInformationByNumber(anInt);
-            List<BlockInformation> bInfoWithSnappy = blockchainWithSnappy.getBlocksInformationByNumber(anInt);
-
-            for (BlockInformation bi : bInfo) {
-                long saveTime = System.nanoTime();
-                blockchain.getBlockByHash(bi.getHash());
-                normalTime += System.nanoTime() - saveTime;
-
-            }
-
-            for (BlockInformation bis : bInfoWithSnappy) {
-                long saveTimeSnappy = System.nanoTime();
-                blockchainWithSnappy.getBlockByHash(bis.getHash());
-                snappyTime += System.nanoTime() - saveTimeSnappy;
-
-            }
-            quantity++;
-        }
-
-        blockNumbers.close();
-        long[] result = new long[2];
-        result[0] = normalTime;
-        result[1] = snappyTime;
-        return result;
-
-        /*System.out.println("Read with normal time: " + normalTime);
-        System.out.println("Read with snappy time: " + snappyTime);
-        System.out.println("Percentage: " + (100 - (snappyTime*100/normalTime)));*/
-
-    }
-
-
-    public void runReadExperiment(int times){
-        long snappyTotalTime = 0;
-        long normalTotalTime = 0;
-
-
-        for (int i = 0; i < times; i++){
-           System.out.println("Time number " + i);
-           long[] result =  compareReads();
-           normalTotalTime += result[0];
-           snappyTotalTime += result[1];
-        }
-
-        System.out.println(times + " times.");
-        System.out.println("Average of time with normal db " + normalTotalTime/times);
-        System.out.println("Average of time with snappy db " + snappyTotalTime/times);
-    }
-
-
-    public void compareBlockchain() {
-
-        Random valueSource = new Random();
-        IntStream blockNumbers = valueSource.ints(VALUES_TO_GENERATE, MIN, MAX);
-        boolean equals = true;
-
-        for (int anInt : blockNumbers.toArray()) {
-
-            List<BlockInformation> bInfo = blockchain.getBlocksInformationByNumber(anInt);
-            List<BlockInformation> bInfoWithSnappy = blockchainWithSnappy.getBlocksInformationByNumber(anInt);
-            byte[] normalBlock = new byte[0];
-            byte[] snappyBlock = new byte[0];
-
-            for (BlockInformation bi : bInfo) {
-                normalBlock = blockchain.getBlockByHash(bi.getHash()).getEncoded();
-            }
-
-            for (BlockInformation bis : bInfoWithSnappy) {
-                snappyBlock = blockchainWithSnappy.getBlockByHash(bis.getHash()).getEncoded();
-
-            }
-
-            equals &= ByteUtil.fastEquals(normalBlock,snappyBlock);
-        }
-        if (equals) {
-            System.out.println("Son iguales");
-        } else {
-            System.out.println("No son iguales");
-        }
     }
 
     public static void main(String[] args) {
